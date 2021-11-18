ca2f571953a2b9424fd0341b9f9ac7ae1d478e91
mehdi <mehdi.salhi@heig-vd.ch>
2021-11-18 11:58:39 +0100

    [feat] implemented message and [test] message test

--- src/test/ApplicationTest.java
+++ src/test/ApplicationTest.java
@@ -1,5 +1,21 @@
 package test;
-import org.junit.jupiter.api.Test;
+import model.mail.*;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
+
+    @Test
+    public void messageShouldHaveTheCorrectReceiversFormat() {
+        Person s = new Person("toto@toto.ch");
+        Person p1 = new Person("tata@tata.ch");
+        Person p2 = new Person("titi@tata.ch");
+
+        Message m = new Message(s, "test subject", "test content", p1, p2);
+        String expected = "tata@tata.ch, titi@tata.ch";
+        assertEquals(expected, m.getReceivers());
+
+    }
+
 }
